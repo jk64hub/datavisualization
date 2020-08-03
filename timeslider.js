7b4c108d3b18057a300e065885b5c293f61e854e
unknown <jk64@MININT-5C75RAP.redmond.corp.microsoft.com>
2020-08-02 18:45:25 -0700

    add time filter dropdown

---
+++ timeslider.js
@@ -0,0 +1,34 @@
+const initializeTimeFilter = () => {
+
+    document.getElementById('timeline_header').innerHTML = ''
+
+    var values = [];
+
+    const onClickHandler = (selectedYear) => {
+        loadBarChart(selectedSector, selectedYear);
+    }
+
+    for (var i = 0; i <= 10; i++)
+        values.push(2009 + i)
+
+    var select = document.createElement("select");
+    select.id = 'yearSelection';
+    select.onchange = onClickHandler;
+
+    for (const val of values) {
+        var option = document.createElement("option");
+        option.value = val;
+        option.text = val;
+        option.selected = val === 2019;
+        select.appendChild(option);
+
+
+    }
+    console.log(select);
+    document.getElementById('timeline_header').append(select);
+
+
+    $('#yearSelection').change(function () {
+        onClickHandler($(this).val());
+    })
+}
